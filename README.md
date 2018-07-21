f079ec7b183c7ec6ac2fc1b95df4d393e574b165
Lipnevich <Nicolay.Lipnevich@gmail.com>
2018-07-21 16:09:12 +0300

    Bot is answering now

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # airdrop-bot
 AirDrop Bot for [Waves online hackaton](https://forum.wavesplatform.com/t/airdrop-bot/1145)
+
+Commands that you need for Firebase
+
+firebase functions:config:set bot.hook="HOOK_URL" bot.token="BOT_TOKEN"
+firebase deploy --only functions
+firebase functions:log
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
 const functions = require('firebase-functions');
+const TelegramBot = require('node-telegram-bot-api');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const token = functions.config().bot.token;
+const url = functions.config().bot.hook;
+const bot = new TelegramBot(token, {webHook: { port: 443 }, polling: false});
+
+exports.hook = functions.https.onRequest((request, response) => {
+    console.log('Request body: ' + JSON.stringify(request.body));
+
+    let message = request.body.message;
+    let chat = message.chat;
+
+    if (chat) {
+        if (chat.type == "private") {
+            bot.sendMessage(chat.id, 'Hei-ho! Please add me to your group in order to start!')
+        }
+    }
+
+    response.status(201).send('Done!');
+});
+
+exports.init = functions.https.onRequest((request, response) => {
+    bot.setWebHook(`${url}/bot${token}`);
+    response.status(201).send('Web hook added!');
+});
+
+
